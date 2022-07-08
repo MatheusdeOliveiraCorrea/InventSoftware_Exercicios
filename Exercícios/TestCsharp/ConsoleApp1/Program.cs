ae30e2c545dff193ac39b535d5b6cbf4d37f800a
MatheusdeOliveiraCorrea <93480352+MatheusdeOliveiraCorrea@users.noreply.github.com>
2022-07-07 23:28:24 -0300

    declarando algumas variaveis

---
+++ Exercícios/TestCsharp/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double m = 0;
+            double c = 0; 
+			double i = 0;
+			double t = 0; 
+			
+			
+            Console.WriteLine("Digite o capital inicial:");
+            c = double.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
